71e42de6860af1d8b88cf1fefda0bca07c9f5980
Christophe Benz <cbenz@easter-eggs.com>
2011-06-06 20:14:16 +0200

    Index view and DB.

--- openchordcharts/__init__.py
+++ openchordcharts/__init__.py
@@ -1,13 +1,29 @@
+from gridfs import GridFS
+import pymongo
+
 from pyramid.config import Configurator
+from pyramid.events import subscriber
+from pyramid.events import NewRequest
+
 from openchordcharts.resources import Root
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(root_factory=Root, settings=settings)
-    config.add_view('openchordcharts.views.my_view',
-                    context='openchordcharts:resources.Root',
-                    renderer='openchordcharts:templates/mytemplate.pt')
+    conn = pymongo.Connection(settings['db_uri'])
+    config.registry.settings['db_conn'] = conn
+    config.add_subscriber(add_mongo_db, NewRequest)
+    config.add_route('index', '/')
+    config.scan('openchordcharts')
     config.add_static_view('static', 'openchordcharts:static')
     return config.make_wsgi_app()
 
+
+# From http://docs.pylonsproject.org/projects/pyramid_cookbook/dev/mongo.html
+def add_mongo_db(event):
+    settings = event.request.registry.settings
+    db = settings['db_conn'][settings['db_name']]
+    event.request.db = db
+    event.request.fs = GridFS(db)
--- openchordcharts/views.py
+++ openchordcharts/views.py
@@ -1,2 +1,6 @@
-def my_view(request):
-    return {'project':'open-chord-charts'}
+from pyramid.view import view_config
+
+
+@view_config(route_name = 'index', renderer = '/index.mako')
+def index(request):
+    return dict()
