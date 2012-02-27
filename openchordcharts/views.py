0661508ccc284185bc1cb280c050a11a64fc522a
Christophe Benz <cbenz@easter-eggs.com>
2012-02-27 15:22:27 +0100

    Add some error handling.

--- openchordcharts/views.py
+++ openchordcharts/views.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from pyramid.exceptions import Forbidden, NotFound
 from pyramid.view import view_config
 
 
 @view_config(route_name='chart', renderer='/chart.mako')
 def chart(request):
     title = request.matchdict.get('title')
+    if not title:
+        return Forbidden()
     chart = request.db.charts.find_one(dict(title=title))
+    if chart is None:
+        return NotFound()
     return dict(
         chart=chart,
         )
