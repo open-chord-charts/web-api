530923fa39609c77b800377e74ae45721e033478
Christophe Benz <christophe.benz@gmail.com>
2012-06-25 22:19:42 +0200

    Simplify condition due to settings validation.

--- openchordcharts/helpers.py
+++ openchordcharts/helpers.py
@@ -30,9 +30,7 @@ def get_login_url(request):
     settings = request.registry.settings
     if settings['authentication.fake_login']:
         return request.route_path('fake_login', _query=dict(callback_path=request.path_qs))
-    elif settings['authentication.openid.application_name'] and \
-        settings['authentication.openid.client_id'] and settings['authentication.openid.client_secret'] and \
-        settings['authentication.openid.provider_url']:
+    elif settings['authentication.openid.application_name']:
         return request.route_path('openidconnect_login', _query=dict(callback_path=request.path_qs))
     elif settings['authentication.localdb_login_enabled']:
         return request.route_path('localdb_login', _query=dict(state=request.path_qs))
