1974206c136ca1705f2032244566aec29e2558d6
Christophe Benz <cbenz@easter-eggs.com>
2012-06-29 16:20:19 +0200

    CHANGE file does not exist.

--- setup.py
+++ setup.py
@@ -30,14 +30,13 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = ['babel', 'biryani', 'formencode', 'pymongo', 'pyramid', 'suq-monpyjama', 'Wannanou>=0.1dev']
 
 setup(name='open-chord-charts',
       version='0.0',
       description='open-chord-charts',
-      long_description=README + '\n\n' + CHANGES,
+      long_description=README,
       classifiers=[
         "Programming Language :: Python",
         "Framework :: Pylons",
