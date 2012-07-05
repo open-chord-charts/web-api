fb4dbdd1234d1f0722406b93e5f968d10417d039
Christophe Benz <cbenz@easter-eggs.com>
2012-07-05 15:56:05 +0200

    Unused import.

--- openchordcharts/views/__init__.py
+++ openchordcharts/views/__init__.py
@@ -23,7 +23,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from pyramid.httpexceptions import HTTPNotFound
+#from pyramid.httpexceptions import HTTPNotFound
 
 
 def cache_manifest(request):
