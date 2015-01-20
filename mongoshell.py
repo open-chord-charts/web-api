991dc8c75433f2c94950b55fbafc284d31c9b6c0
Christophe Benz <christophe.benz@data.gouv.fr>
2015-01-20 18:44:48 +0100

    Change section name

--- mongoshell.py
+++ mongoshell.py
@@ -16,6 +16,6 @@ from openchordcharts_api.model import Account, Chart  # noqa
 base_dir = os.path.abspath(os.path.dirname(__file__))
 conf_file_name = 'production.ini' if os.path.exists('production.ini') else 'development.ini'
 conf_file_path = os.path.join(base_dir, conf_file_name)
-app = loadapp(u'config:{}#api'.format(conf_file_path))
+app = loadapp(u'config:{}#main'.format(conf_file_path))
 db = environment.db
 ObjectId = bson.ObjectId
